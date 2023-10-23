7ea0391515d05c442295d91c5c16428f61b375dd
Lorenzo Baldassari <lorebalda93@gmail.com>
2023-10-23 15:36:10 +0200

    exercise finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./component/Button";
+import ImageComponent from "./component/ImageComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button string="Funzione eseguita" />
+      <ImageComponent src="https://placekitten.com/200" alt="kittens image" />
     </div>
   );
 }
---
+++ src/component/Button.jsx
@@ -0,0 +1,6 @@
+const Button = function (props)
+{
+return <button>ciao {props.string}</button>
+}
+
+export default Button
